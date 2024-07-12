ad83feb199f079d381dd04b08734d26052699716
Huywb <pthhuy1306@gmail.com>
2024-07-12 17:07:35 +0700

    clerk update

--- libs/actions/user.js
+++ libs/actions/user.js
@@ -4,20 +4,21 @@ import { connectDB } from "../mongoose/connetDB";
 export const createOrUpdateUser = async(id, first_name, last_name , image_url, email_addresses, username) =>{
     try {
         await connectDB()
-        console.log('123')
-        const UserId = await User.findOne({clerkId: id})
-        console.log('id',UserId)
-        if(UserId){
-            const data = await User.create({
-                clerkId: id,
-                firstName:first_name,
-                lastName: last_name,
-                profilePhoto : image_url,
-                email: email_addresses,
-                userName : username
-            })
-            await data.save()
-        }
+        const user = await User.findOneAndUpdate(
+            {clerkId: id},
+            {
+                $set:{
+                    firstName:first_name,
+                    lastName: last_name,
+                    profilePhoto : image_url,
+                    email: email_addresses,
+                    userName : username
+                }
+            },
+            {upsert:true,new:true}
+        )
+        await user.save()
+        
     } catch (error) {
         console.log('error',error)
     }
